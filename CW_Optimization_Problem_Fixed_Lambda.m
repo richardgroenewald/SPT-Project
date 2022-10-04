7d0e6a25ba66e9c0b4cb9781f429dce67da33ae6
Richard Groenewald <richard.groenewald@columbia.edu>
2022-10-03 22:25:00 -0400

    October 3rd update

--- CW_Optimization_Problem_Fixed_Lambda.m
+++ CW_Optimization_Problem_Fixed_Lambda.m
@@ -30,6 +30,10 @@ n = dims(1);
 %index of the zero value for the functions l_j
 zero_ind = find(0.5 == grid,1);
 
+%checking with print statements
+%fprintf('n is %d\n ', n);
+%fprintf('T is %d\n ', T);
+
 category = 1:n;
 %vector to add to indices below
 for i = 1:n
@@ -51,23 +55,6 @@ dl_indices = indices + adj;
 
 %initialize lambda, assigned for each category
 lambda = repmat(1/n, J, 1);
-l = zeros(J*d, 1);
-obj = inf;
-
-%for entering while loop
-l_diff = inf;
-lambda_diff = inf;
-obj_diff = inf;
-
-%tolerance/termination condition, k to track number of iterations
-tol = 1e-02;
-k = 1;
-
-while obj_diff > tol && k <=1 %l_diff > tol || lambda_diff > tol
-
-l_old = l;
-lambda_old = lambda;
-obj_old = obj;
 
 cvx_solver mosek;
 
@@ -86,8 +73,7 @@ cvx_begin
     end
 
     %build objective function - multiplying lambdas by function derivatives by delta_pq and summing by column
-    dlogv = log(1 + sum(delta_pq .* lambdas .* dl(dl_indices) ./ int_lengths(indices), 1));
-    obj = sum(dlogv);
+    obj = sum(log(1 + sum(delta_pq .* lambdas .* dl(dl_indices) ./ int_lengths(indices), 1)));
 
     maximize(obj)
     subject to
@@ -95,7 +81,7 @@ cvx_begin
             for i = 1:(d-2)
                 %constraint (3.1) - exponential concavity approximation
                 w = int_lengths(i)/(int_lengths(i+1) + int_lengths(i));
-                -l((j-1)*d + i+1) + log( w*exp(l((j-1)*d + i+2)) + (1-w)*exp(l((j-1)*d + i)) ) <= 0;
+                -l((j-1)*d + i+1) + log (w*exp(l((j-1)*d + i+2)) + (1-w)*exp(l((j-1)*d + i)) ) <= 0;
 
                 %constraint (3.2) - beta Lipschitz derivatives approximation
                 dl((j-1)*d + i+1)/int_lengths(i+1) - dl((j-1)*d + i)/int_lengths(i) >= -beta*(int_lengths(i+1)+int_lengths(i))/2;
@@ -109,42 +95,5 @@ cvx_begin
             l((j-1)*d + zero_ind) == 0;
         end
 cvx_end
-
-z_1 = cvx_optval;
-
-%optimization problem for lambda
-cvx_begin
-    variable lambda(J)
-    %break into vector corresponding to each ranked stock
-    lambdas = lambda(category);
-    %single category case - lambda = 1/n
-    if J == 1
-        lambdas = lambda(category)';
-    end
-
-    dlogv = log(1 + sum(delta_pq .* repmat(lambdas, 1, T) .* dl(dl_indices) ./ int_lengths(indices), 1));
-    disp(length(dlogv));
-    obj = sum(dlogv);
-
-    maximize(obj)
-    subject to
-        lambda >= 0;
-        sum(lambdas) == 1;
-cvx_end
-
-%compute differences between iterations
-l_diff = norm(l - l_old);
-lambda_diff = norm(lambda - lambda_old);
-obj_diff = norm(obj - obj_old);
-
-fprintf("Iteration number: %d\n ", k);
-fprintf("l_diff: %d\n ", l_diff);
-fprintf("lambda_diff: %d\n", lambda_diff);
-fprintf("obj_diff: %d\n", obj_diff);
-fprintf("-------------------");
-
-k = k+1;
-
-end
 
 toc
